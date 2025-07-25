77daa158efe9c733e1b1aca936032a3e39bf96da
abdelrhman3iad <abdelrhmanaiid@wowws.net>
2025-07-25 08:03:46 +0200

    Request and Response Objects

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const http = require('http');
 // Returns instance of http.Server Object
 let server = http.createServer((req,res)=>{
     console.log(req);
+    console.log("Url : " , req.url , "Method : " , req.method , "Header : " , req.headers);
+    // process.exit();
 });
 
 server.listen(8000);
