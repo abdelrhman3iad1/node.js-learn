453bc202d0289c9edb7b8016ea53555096b0c449
abdelrhman3iad <abdelrhmanaiid@wowws.net>
2025-07-25 08:26:34 +0200

    handle routing and html responses

--- app.js
+++ app.js
@@ -5,8 +5,25 @@ const http = require('http');
 
 // Returns instance of http.Server Object
 let server = http.createServer((req,res)=>{
-    console.log(req);
-    console.log("Url : " , req.url , "Method : " , req.method , "Header : " , req.headers);
+    // console.log(req);
+    // console.log("Url : " , req.url , "Method : " , req.method , "Header : " , req.headers);
+    if (req.url === "/"){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+            res.write('<body>');
+                res.write("<form action='/message' method='post'><input type='text'><button type='submit'>Submit</button></form>");
+            res.write('</body>');
+        res.write('</html>');
+        return res.end();
+    }
+    
+    res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+            res.write('<body>');
+                res.write('Hahaha');
+            res.write('</body>');
+        res.write('</html>');
+    res.end();
     // process.exit();
 });
 
