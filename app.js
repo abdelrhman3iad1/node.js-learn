3dec0b3aea50afcb78dbc72aa1408d89549c193c
abdelrhman3iad <abdelrhmanaiid@wowws.net>
2025-07-26 14:38:30 +0200

    Deals With Routing and writing HTML response and data as chunks and compine them with buffer and export a text file using fs package

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 // Core Module 
 const http = require('http');
+const fs = require('fs');
 
 
 // Returns instance of http.Server Object
@@ -11,12 +12,40 @@ let server = http.createServer((req,res)=>{
         res.setHeader('Content-Type','text/html');
         res.write('<html>');
             res.write('<body>');
-                res.write("<form action='/message' method='post'><input type='text'><button type='submit'>Submit</button></form>");
+                res.write("<form action='/message' method='POST'><input name='message' type='text'><button type='submit'>Submit</button></form>");
             res.write('</body>');
         res.write('</html>');
         return res.end();
     }
-    
+
+    // Handling with Routing and Create a file and redirect to specific Location (route)
+    //  with specific statusCode 
+
+    /**
+     * nodejs handling data in req by stream to send the data (text / images / files ) as chunks 
+     * each chuck can't handle it alone
+     * we use buffer that holds several chunks
+     * req.on the method that handle these chunks 
+     * 
+     * then read it by Buffer Object
+     */
+    if (req.url === "/message" && req.method ==='POST'){
+        const data = [];
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            data.push(chunk);
+        });
+        req.on('end',()=>{
+            const parsedData = Buffer.concat(data).toString(); // BC ITS TEXT 
+            const message = parsedData.split('=')[1];
+            console.log(message);
+            fs.writeFileSync('message.txt',message);
+        })
+        res.statusCode = 302;
+        res.setHeader('Location' ,'/');
+        return res.end();
+    }
+
     res.setHeader('Content-Type','text/html');
         res.write('<html>');
             res.write('<body>');
